5de740b97f31a8e6ed9bad3f5ff43e0081a896cb
shailesh897 <shailesh.p897@gmail.com>
2020-01-08 01:55:25 +0530

    Added Wrong For Loop

--- FirstPythonFileTry.py
+++ FirstPythonFileTry.py
@@ -1,3 +1,5 @@
 print("Hi this is my first Python file on github")
 while True:
     print(" Eat\n Drink\n Think\n Code\n Sleep\n Repeat")
+for True:
+     print(" Eat\n Drink\n Think\n Code\n Sleep\n Repeat")
